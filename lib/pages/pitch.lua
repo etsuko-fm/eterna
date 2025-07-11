0a1f856722a9b860a7468def15151b871264d822
Ruben <rubencronie@gmail.com>
2025-07-11 23:15:17 +0100

    move pitch state to central state

--- lib/pages/pitch.lua
+++ lib/pages/pitch.lua
@@ -5,62 +5,8 @@ local page_name = "Playback"
 local misc_util = include("bits/lib/util/misc")
 local page
 
-local PARAM_ID_DIRECTION = "pitch_direction"
-local PARAM_ID_QUANTIZE = "pitch_quantize"
-local PARAM_ID_CENTER = "pitch_center"
-local PARAM_ID_SPREAD = "pitch_spread"
-
--- voice directions; also used for other pages, hence global
-function get_voice_dir_param_id(i)
-  return "pitch_v" .. i .. "_dir"
-end
-
-local CENTER_MIN = -2
-local CENTER_MAX = 2
-local CENTER_QUANTUM = 1 / 120
-local CENTER_QUANTUM_QUANTIZED = 1.0
-
-local SPREAD_MIN = -2
-local SPREAD_MAX = 2
-local SPREAD_MIN_QUANTIZED = -2
-local SPREAD_MAX_QUANTIZED = 2
-local SPREAD_QUNATUM = 0.01
-local SPREAD_QUNATUM_QUANTIZED = 0.5
-
-local FWD = "FWD"
-local REV = "REV"
-local FWD_REV = "FWD+REV"
-local PLAYBACK_TABLE = { FWD, REV, FWD_REV }
-
-local OFF = "OFF"
-local OCTAVES = "OCTAV"
-local QUANTIZE_TABLE = { OFF, OCTAVES }
-local QUANTIZE_DEFAULT = 2 -- octave quantization by default
-
-local controlspec_center = controlspec.def {
-    min = CENTER_MIN,                   -- the minimum value
-    max = CENTER_MAX,                   -- the maximum value
-    warp = 'lin',                       -- a shaping option for the raw value
-    step = 1 / 120,                     -- output value quantization
-    default = 0.0,                      -- default value
-    units = '',                         -- displayed on PARAMS UI
-    quantum = CENTER_QUANTUM_QUANTIZED, -- each delta will change raw value by this much
-    wrap = false                        -- wrap around on overflow (true) or clamp (false)
-}
-
-local controlspec_spread = controlspec.def {
-    min = SPREAD_MIN_QUANTIZED,         -- the minimum value
-    max = SPREAD_MAX_QUANTIZED,         -- the maximum value
-    warp = 'lin',                       -- a shaping option for the raw value
-    step = 0.01,                        -- output value quantization
-    default = 0.0,                      -- default value
-    units = '',                         -- displayed on PARAMS UI
-    quantum = SPREAD_QUNATUM_QUANTIZED, -- each delta will change raw value by this much
-    wrap = false                        -- wrap around on overflow (true) or clamp (false)
-}
-
 local function calculate_rates()
-    local quantize = QUANTIZE_TABLE[params:get(PARAM_ID_QUANTIZE)]
+    local quantize = QUANTIZE_TABLE[params:get(ID_PITCH_QUANTIZE)]
     -- recalculate softcut playback rates, taking into account quantize, spread, center, direction
     for i = 0, 5 do
         -- map 6 values as equally spread angles on a (virtual) circle, by using radians (fraction * 2PI)
@@ -72,10 +18,10 @@ local function calculate_rates()
         local extend = 2 --2.67 -- manually tuned, 2.7 is also nice
 
         -- here pitch is still a linear value, representing steps on the slider
-        local pitch = math.sin(radians * extend) * params:get(PARAM_ID_SPREAD)
+        local pitch = math.sin(radians * extend) * params:get(ID_PITCH_SPREAD)
 
         -- double to increase range, we'll use half the range for reverse playback (-4 < pitch < 0) and half for forward (0 < pitch < 4)
-        pitch = pitch + params:get(PARAM_ID_CENTER)
+        pitch = pitch + params:get(ID_PITCH_CENTER)
 
         if quantize ~= OFF then
             -- quantize to integers between -2 and 2,because 2^[-2|-1|0|1|2] gives quantized rates from 0.25 to 4
@@ -126,19 +72,10 @@ local function update_playback_dir(new_val)
 end
 
 local function add_params()
-    params:add_separator("PLAYBACK_RATES", "PLAYBACK RATES")
-
-    params:add_option(PARAM_ID_QUANTIZE, 'quantize', QUANTIZE_TABLE, QUANTIZE_DEFAULT)
-    params:set_action(PARAM_ID_QUANTIZE, action_quantize)
-
-    params:add_control(PARAM_ID_CENTER, "center", controlspec_center)
-    params:set_action(PARAM_ID_CENTER, calculate_rates)
-
-    params:add_control(PARAM_ID_SPREAD, "spread", controlspec_spread)
-    params:set_action(PARAM_ID_SPREAD, calculate_rates)
-
-    params:add_option(PARAM_ID_DIRECTION, "direction", PLAYBACK_TABLE, 1)
-    params:set_action(PARAM_ID_DIRECTION, update_playback_dir)
+    params:set_action(ID_PITCH_QUANTIZE, action_quantize)
+    params:set_action(ID_PITCH_CENTER, calculate_rates)
+    params:set_action(ID_PITCH_SPREAD, calculate_rates)
+    params:set_action(ID_PITCH_DIRECTION, update_playback_dir)
 
     -- voice directions (fwd/rev/both)
     for voice = 1, 6 do
@@ -146,45 +83,44 @@ local function add_params()
         params:add_option(param_id, param_id, PLAYBACK_TABLE, 1)
         params:hide(param_id)
     end
-
 end
 
 function action_quantize(v)
     if QUANTIZE_TABLE[v] == OCTAVES then
-        params:set(PARAM_ID_CENTER, math.floor(params:get(PARAM_ID_CENTER) + .5))
-        controlspec_center.quantum = CENTER_QUANTUM_QUANTIZED;
-        controlspec_spread.quantum = SPREAD_QUNATUM_QUANTIZED
-        controlspec_spread.minval = SPREAD_MIN_QUANTIZED
-        controlspec_spread.maxval = SPREAD_MAX_QUANTIZED
-        params:set(PARAM_ID_SPREAD, math.floor(params:get(PARAM_ID_SPREAD) + .5))
+        params:set(ID_PITCH_CENTER, math.floor(params:get(ID_PITCH_CENTER) + .5))
+        controlspec_center.quantum = PITCH_CENTER_QUANTUM_QNT;
+        controlspec_spread.quantum = PITCH_SPREAD_QUANTUM_QNT
+        controlspec_spread.minval = PITCH_SPREAD_MIN_QNT
+        controlspec_spread.maxval = PITCH_SPREAD_MAX_QNT
+        params:set(ID_PITCH_SPREAD, math.floor(params:get(ID_PITCH_SPREAD) + .5))
     else
-        controlspec_center.quantum = CENTER_QUANTUM;
-        controlspec_spread.quantum = SPREAD_QUNATUM;
-        controlspec_spread.minval = SPREAD_MIN
-        controlspec_spread.maxval = SPREAD_MAX
+        controlspec_center.quantum = PITCH_CENTER_QUANTUM;
+        controlspec_spread.quantum = PITCH_SPREAD_QUANTUM;
+        controlspec_spread.minval = PITCH_SPREAD_MIN
+        controlspec_spread.maxval = PITCH_SPREAD_MAX
     end
     calculate_rates()
 end
 
 local function cycle_direction()
-    local new = util.wrap(params:get(PARAM_ID_DIRECTION) + 1, 1, 3)
-    params:set(PARAM_ID_DIRECTION, new)
+    local new = util.wrap(params:get(ID_PITCH_DIRECTION) + 1, 1, 3)
+    params:set(ID_PITCH_DIRECTION, new)
 end
 
 
 local function toggle_quantize()
-    local old = params:get(PARAM_ID_QUANTIZE)
+    local old = params:get(ID_PITCH_QUANTIZE)
     local new = util.wrap(old + 1, 1, #QUANTIZE_TABLE)
-    params:set(PARAM_ID_QUANTIZE, new)
+    params:set(ID_PITCH_QUANTIZE, new)
 end
 
 local function adjust_center(d)
-    params:set(PARAM_ID_CENTER, params:get(PARAM_ID_CENTER) + d * controlspec_center.quantum, false)
-    page.pitch_graph.center = params:get(PARAM_ID_CENTER) * -2 -- why *-2?
+    params:set(ID_PITCH_CENTER, params:get(ID_PITCH_CENTER) + d * controlspec_center.quantum, false)
+    page.pitch_graph.center = params:get(ID_PITCH_CENTER) * -2 -- why *-2?
 end
 
 local function adjust_spread(d)
-    params:set(PARAM_ID_SPREAD, params:get(PARAM_ID_SPREAD) + d * controlspec_spread.quantum)
+    params:set(ID_PITCH_SPREAD, params:get(ID_PITCH_SPREAD) + d * controlspec_spread.quantum)
 end
 
 page = Page:create({
@@ -201,17 +137,17 @@ page = Page:create({
 
 function page:render()
     page.window:render()
-    page.footer.button_text.k2.value = PLAYBACK_TABLE[params:get(PARAM_ID_DIRECTION)]
-    page.footer.button_text.k3.value = QUANTIZE_TABLE[params:get(PARAM_ID_QUANTIZE)]
+    page.footer.button_text.k2.value = PLAYBACK_TABLE[params:get(ID_PITCH_DIRECTION)]
+    page.footer.button_text.k3.value = QUANTIZE_TABLE[params:get(ID_PITCH_QUANTIZE)]
 
     -- convert -3/+3 range to -36/+36 rounded to 1 decimal
     page.footer.button_text.e2.value = misc_util.trim(tostring(
-        math.floor(params:get(PARAM_ID_CENTER) * 1200 + .5) / 100
+        math.floor(params:get(ID_PITCH_CENTER) * 1200 + .5) / 100
     ), 5)
 
     -- Round value to 2 decimals
     page.footer.button_text.e3.value = misc_util.trim(tostring(
-        math.floor(params:get(PARAM_ID_SPREAD) * 100 + .5) / 100
+        math.floor(params:get(ID_PITCH_SPREAD) * 100 + .5) / 100
     ), 5)
 
     page.pitch_graph:render()
--- lib/state.lua
+++ lib/state.lua
@@ -42,12 +42,72 @@ controlspec_amp = controlspec.def {
     wrap = false     -- wrap around on overflow (true) or clamp (false)
 }
 
-local default_rate_index = 20
-local default_rate = lfo_util.lfo_period_values[default_rate_index]
+local LEVELS_LFO_DEFAULT_RATE_INDEX = 20
+local LEVELS_LFO_DEFAULT_RATE = lfo_util.lfo_period_values[LEVELS_LFO_DEFAULT_RATE_INDEX]
 
 params:add_separator("BITS_LEVELS", "LEVELS")
 params:add_binary(ID_LEVELS_LFO_ENABLED, "LFO enabled", "toggle", 0)
 params:add_option(ID_LEVELS_LFO_SHAPE, "LFO shape", LEVELS_LFO_SHAPES, 1)
-params:add_option(ID_LEVELS_LFO_RATE, "LFO rate", lfo_util.lfo_period_labels, default_rate)
+params:add_option(ID_LEVELS_LFO_RATE, "LFO rate", lfo_util.lfo_period_labels, LEVELS_LFO_DEFAULT_RATE_INDEX)
 params:add_control(ID_LEVELS_POS, "position", controlspec_pos)
 params:add_control(ID_LEVELS_AMP, "amp", controlspec_amp)
+
+ID_PITCH_DIRECTION = "pitch_direction"
+ID_PITCH_QUANTIZE = "pitch_quantize"
+ID_PITCH_CENTER = "pitch_center"
+ID_PITCH_SPREAD = "pitch_spread"
+
+-- voice directions; also used for other pages, hence global
+function get_voice_dir_param_id(i)
+  return "pitch_v" .. i .. "_dir"
+end
+
+PITCH_CENTER_MIN = -2
+PITCH_CENTER_MAX = 2
+PITCH_CENTER_QUANTUM = 1 / 120
+PITCH_CENTER_QUANTUM_QNT = 1.0
+
+PITCH_SPREAD_MIN = -2
+PITCH_SPREAD_MAX = 2
+PITCH_SPREAD_MIN_QNT = -2
+PITCH_SPREAD_MAX_QNT = 2
+PITCH_SPREAD_QUANTUM = 0.01
+PITCH_SPREAD_QUANTUM_QNT = 0.5
+
+FWD = "FWD"
+REV = "REV"
+FWD_REV = "FWD+REV"
+PLAYBACK_TABLE = { FWD, REV, FWD_REV }
+
+OFF = "OFF"
+OCTAVES = "OCTAV"
+QUANTIZE_TABLE = { OFF, OCTAVES }
+QUANTIZE_DEFAULT = 2 -- octave quantization by default
+
+controlspec_center = controlspec.def {
+    min = PITCH_CENTER_MIN,                   -- the minimum value
+    max = PITCH_CENTER_MAX,                   -- the maximum value
+    warp = 'lin',                       -- a shaping option for the raw value
+    step = 1 / 120,                     -- output value quantization
+    default = 0.0,                      -- default value
+    units = '',                         -- displayed on PARAMS UI
+    quantum = PITCH_CENTER_QUANTUM_QNT, -- each delta will change raw value by this much
+    wrap = false                        -- wrap around on overflow (true) or clamp (false)
+}
+
+controlspec_spread = controlspec.def {
+    min = PITCH_SPREAD_MIN_QNT,         -- the minimum value
+    max = PITCH_SPREAD_MAX_QNT,         -- the maximum value
+    warp = 'lin',                       -- a shaping option for the raw value
+    step = 0.01,                        -- output value quantization
+    default = 0.0,                      -- default value
+    units = '',                         -- displayed on PARAMS UI
+    quantum = PITCH_SPREAD_QUANTUM_QNT, -- each delta will change raw value by this much
+    wrap = false                        -- wrap around on overflow (true) or clamp (false)
+}
+
+params:add_separator("PLAYBACK_RATES", "PLAYBACK RATES")
+params:add_option(ID_PITCH_QUANTIZE, 'quantize', QUANTIZE_TABLE, QUANTIZE_DEFAULT)
+params:add_control(ID_PITCH_CENTER, "center", controlspec_center)
+params:add_control(ID_PITCH_SPREAD, "spread", controlspec_spread)
+params:add_option(ID_PITCH_DIRECTION, "direction", PLAYBACK_TABLE, 1)
