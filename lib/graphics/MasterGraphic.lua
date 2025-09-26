40ccbcbc1a8c3eef0f85db12a069485fe84af609
Ruben <rubencronie@gmail.com>
2025-09-26 16:20:59 +0200

    experiment with lissajous curve

--- lib/graphics/MasterGraphic.lua
+++ lib/graphics/MasterGraphic.lua
@@ -4,6 +4,7 @@ MasterGraphic = {
     hide = false,
     pre_comp_levels = {0,0},
     post_comp_levels = {0,0},
+    lissajous_buf = {},
 }
 
 function MasterGraphic:new(o)
@@ -18,6 +19,27 @@ function MasterGraphic:new(o)
     return o
 end
 
+function MasterGraphic:add_sample(l, r)
+  table.insert(self.lissajous_buf, {l, r})
+  if #self.lissajous_buf > 30 then
+    table.remove(self.lissajous_buf, 1)
+  end
+end
+
+function MasterGraphic:draw_lissajous()
+  local center_x = 64
+  local center_y = 32
+  local scale = 30
+
+  for i, s in ipairs(self.lissajous_buf) do
+    screen.level(i)
+    local x = center_x + s[1] * scale
+    local y = center_y - s[2] * scale
+    screen.pixel(x, y)
+  end
+  screen.stroke()
+end
+
 function MasterGraphic:render()
     if self.hide then return end
     screen.level(15)
@@ -28,7 +50,6 @@ function MasterGraphic:render()
     screen.rect(38, 40, 4, self.pre_comp_levels[2] * -20)
     screen.fill()
     screen.move(64,32)
-    screen.text_center("hello world!")
 
     -- post levels
     screen.rect(48, 40, 4, self.post_comp_levels[1] * -20)
@@ -36,6 +57,11 @@ function MasterGraphic:render()
     screen.rect(54, 40, 4, self.post_comp_levels[2] * -20)
     screen.fill()
     screen.move(64,32)
+
+    -- lissajous
+    -- self:add_sample(self.post_comp_levels[1], self.post_comp_levels[2])
+    -- self:draw_lissajous()
+
 end
 
 return MasterGraphic
