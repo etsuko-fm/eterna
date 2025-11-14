76253679362a4a606088b1549e70c55817cf9635
Ruben <rubencronie@gmail.com>
2025-11-15 00:03:39 +0100

    Fix echo graphic bug

--- lib/parameters.lua
+++ lib/parameters.lua
@@ -331,8 +331,8 @@ controlspec_hpf_freq_mod = controlspec.def {
 local componentid = "echo_"
 
 ID_ECHO_TIME = componentid .. "time"
-ECHO_TIME_AMOUNTS = { 0.0625, 0.125, 0.1875, 0.25, 0.375, 0.5, 0.625, 0.75, 1, 1.25 }
-ECHO_TIME_NAMES = {"1/64", "1/32", "1/32D", "1/16", "1/16D", "1/8", "5/32", "1/8D", "1/4", "5/16" }
+ECHO_TIME_AMOUNTS = { 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.5, 0.625, 0.75}
+ECHO_TIME_NAMES =   {"1/64", "1/32", "1/32D", "1/16", "5/64", "1/16D", "1/8", "5/32", "1/8D"}
 
 ---
 --- MASTER params
