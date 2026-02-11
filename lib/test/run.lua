33b7f280f11a13362d6335d13c00fb46fc4f633c
Cronie, Ruben <rubencronie@kpn.com>
2026-02-11 11:44:07 +0100

    wip: add some tests for grid

--- lib/test/run.lua
+++ lib/test/run.lua
@@ -3,11 +3,10 @@ local luaunit = require 'test.luaunit'
 TestCycleParam = include(from_root("lib/test/util/misc/test_cycle_param"))
 TestExplin = include(from_root("lib/test/util/misc/test_explin"))
 TestToggleParam = include(from_root("lib/test/util/misc/test_toggle_param"))
-
-local tests = {TestCycleParam, TestExplin, TestToggleParam}
+TestGrid = include(from_root("lib/test/test_grid"))
 
 local runner = luaunit.LuaUnit.new()
 runner:setOutputType('text')
-runner:runSuite('TestCycleParam', 'TestExplin', 'TestToggleParam')
+runner:runSuite('TestCycleParam', 'TestExplin', 'TestToggleParam', 'TestGrid')
 
 -- dofile('/home/we/dust/code/eterna/lib/test/run.lua')
